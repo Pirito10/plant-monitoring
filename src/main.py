b0b972267a21848dd7908454e7d78d416a9549e4
Pirito10 <aaronriveiro@gmail.com>
2025-11-03 00:22:41 +0100

    add blocking scheduler

--- src/main.py
+++ src/main.py
@@ -1,14 +1,7 @@
-import time
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 from src.camera import take_photo
 
-scheduler = BackgroundScheduler()
+scheduler = BlockingScheduler()
 scheduler.add_job(take_photo, "cron", hour="10,18")
 scheduler.start()
-
-try:
-    while True:
-        time.sleep(3600)
-except KeyboardInterrupt:
-    scheduler.shutdown()
