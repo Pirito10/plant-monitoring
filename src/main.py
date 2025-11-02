42817e1d8b2abd75005d759828a78129385301c3
Pirito10 <aaronriveiro@gmail.com>
2025-11-02 23:59:26 +0100

    add basic scheduler

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+
 from src.camera import take_photo
 
+scheduler = BackgroundScheduler()
+scheduler.add_job(take_photo, "cron", hour="10,18")
+scheduler.start()
+
 try:
-    take_photo()
-except Exception as e:
-    pass
+    while True:
+        time.sleep(3600)
+except KeyboardInterrupt:
+    scheduler.shutdown()
