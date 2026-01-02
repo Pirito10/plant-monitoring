d188f1cf04fce9e9bbe24a11d44c77469f2361a6
Pirito10 <aaronriveiro@gmail.com>
2026-01-02 01:41:05 +0100

    rewrite main imports

--- src/main.py
+++ src/main.py
@@ -1,19 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from src.config import load_config
-from src.camera import take_photo
-from src.moisture_sensor import read_soil_moisture
-from src.led_rgb import update_led
+from src import config, camera, moisture, led_rgb
 
 # Cargamos la configuración
-cfg = load_config()
+cfg = config.load_config()
 
 # Creamos el planificador de tareas
 scheduler = BlockingScheduler()
 
 # Programamos la toma de fotos
 scheduler.add_job(
-    take_photo,
+    camera.take_photo,
     "cron",
     hour=",".join(map(str, cfg["scheduler"]["hours"])),
     args=[
@@ -25,8 +22,8 @@ scheduler.add_job(
 
 #! Temporal
 def job_read_moisture():
-    moisture = read_soil_moisture(cfg["moisture"]["raw_dry"], cfg["moisture"]["raw_wet"])
-    update_led(moisture, 50)
+    moisture = moisture.read_soil_moisture(cfg["moisture"]["raw_dry"], cfg["moisture"]["raw_wet"])
+    led_rgb.update_led(moisture, cfg["moisture"]["optimal"])
     print(f"Humedad suelo: {moisture:5.1f}%")
 
 # Programamos la lectura del sensor de humedad
