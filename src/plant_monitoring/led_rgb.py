26ad2a7ee7549cf6267803af2099f75afbd0475a
Pirito10 <aaronriveiro@gmail.com>
2026-05-23 13:15:17 +0200

    refactor: migrate from RPi.GPIO to gpiozero

--- src/plant_monitoring/led_rgb.py
+++ src/plant_monitoring/led_rgb.py
@@ -1,24 +1,11 @@
-import RPi.GPIO as GPIO
+from gpiozero import RGBLED
 
 
 class LedRGB:
     # Inicializamos el LED RGB
-    def __init__(self, pin_r, pin_g, pin_b, pwm_freq, optimal_moisture):
-        # Inicializamos los pines GPIO
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(pin_r, GPIO.OUT)
-        GPIO.setup(pin_g, GPIO.OUT)
-        GPIO.setup(pin_b, GPIO.OUT)
-
-        # Configuramos la frecuencia PWM para cada color
-        self.pwm_r = GPIO.PWM(pin_r, pwm_freq)
-        self.pwm_g = GPIO.PWM(pin_g, pwm_freq)
-        self.pwm_b = GPIO.PWM(pin_b, pwm_freq)
-
-        # Apagamos el LED al inicio
-        self.pwm_r.start(0)
-        self.pwm_g.start(0)
-        self.pwm_b.start(0)
+    def __init__(self, pin_r, pin_g, pin_b, optimal_moisture):
+        # Inicializamos el LED RGB
+        self.led = RGBLED(red=pin_r, green=pin_g, blue=pin_b)
 
         # Guardamos el valor óptimo de humedad
         self.optimal_moisture = optimal_moisture
@@ -35,28 +22,25 @@ class LedRGB:
             # Calculamos el punto dentro del rango
             t = m / opt
             # Calculamos los valores RGB
-            r = int(255 * (1 - t))
-            g = int(255 * t)
-            b = 0
+            r = 1 - t
+            g = t
+            b = 0.0
 
         # Si hay humedad alta, el color sale entre verde y azul
         else:
             # Calculamos el punto dentro del rango
             t = (m - opt) / (1 - opt)
             # Calculamos los valores RGB
-            r = 0
-            g = int(255 * (1 - t))
-            b = int(255 * t)
+            r = 0.0
+            g = 1 - t
+            b = t
 
         # Devolvemos los valores RGB
         return r, g, b
 
     # Función para establecer el color del LED RGB
     def _set_color(self, r, g, b):
-        # Ajustamos los valores PWM para cada color
-        self.pwm_r.ChangeDutyCycle(r / 255 * 100)
-        self.pwm_g.ChangeDutyCycle(g / 255 * 100)
-        self.pwm_b.ChangeDutyCycle(b / 255 * 100)
+        self.led.color = (r, g, b)
 
     # Función para actualizar el color del LED
     def update(self, moisture):
